4c02ff706533d5896a14a00948a612c22cd1a170
EdgarMeneses <edgar@ant.com.mx>
2021-04-28 22:23:23 -0500

    Main Layout seccion 2 y footer

--- front/src/pages/Index.vue
+++ front/src/pages/Index.vue
@@ -52,6 +52,121 @@
           </div>
         </div>
     </div>
+    <div class="row">
+      <div class="col-md-12 q-pa-xl">
+        <div class="text-h4 text-center">
+          <i><b>¿POR QUÉ ELEGIR NUESTRA OPCIÓN?</b> </i>
+        </div>
+      </div>
+      <div class="col-md-6 q-pa-xl text-right">
+          <q-img
+          src="../assets/iconos/rapidex.png"
+          style="width: 15%"
+          >
+          </q-img><br>
+          <div for="" class="q-pt-md text-h6">Rápido aprendizaje</div>
+      </div>
+      <div class="col-md-6 q-pa-xl text-left">
+          <q-img
+          src="../assets/iconos/2metas.png"
+          style="width: 15%"
+          >
+          </q-img><br>
+          <div for="" class="q-pt-md text-h6">Trabajo por objetivos</div>
+      </div>
+      <!-- Parte 2 de imagenes -->
+      <div class="col-md-5 text-right">
+          <q-img
+          src="../assets/iconos/3compromiso.png"
+          style="width: 15%"
+          >
+          </q-img><br>
+          <div for="" class="q-pt-md text-h6">Soporte garantizado</div>
+      </div>
+      <div class="col-md-2 text-center">
+          <q-img
+          src="../assets/iconos/4certificado.png"
+          style="width: 37%"
+          >
+          </q-img><br>
+          <div for="" class="q-pt-md text-h6">Certificación</div>
+      </div>
+      <div class="col-md-5 text-left">
+          <q-img
+          src="../assets/iconos/5facilacceso.png"
+          style="width: 15%"
+          >
+          </q-img><br>
+          <div for="" class="q-pt-md text-h6">Facíl acceso a los cursos</div>
+      </div>
+      <div class="col-md-12 q-pa-xl">
+        <div class="text-h4 text-center">
+          <i><b>CONOCE NUESTRA HISTORIA</b> </i><br>
+          <q-btn label="Ver más" color="pink" rounded no-caps class="q-mt-md"></q-btn>
+        </div>
+      </div>
+    </div>
+    <!-- Footer -->
+    <div class="row bg-dark text-white text-h5">
+      <!-- Encabezados -->
+      <div class="col-md-1"></div>
+      <div class="col-md-3 q-pt-xl q-pl-md">
+          <img src="../assets/header.png" height="50px" @click="main()">
+      </div>
+      <div class="col-md-4 q-pt-xl text-center">
+         <i>REDES SOCIALES</i>
+      </div>
+      <div class="col-md-4 q-pt-xl text-center">
+         <i>ACERCA DE</i>
+      </div>
+      <!-- Datos -->
+      <div class="col-md-1"></div>
+      <div class="col-md-3  q-pl-md q-pt-md text-justify" style="font-size: 15px; line-height: 20px">
+        <p>
+          Prisciliano Sánchez #1105,<br>
+          Col. Americana, <br>
+          Guadalajara, Jalisco. México. <br>
+          322 192 1823 <br>
+          info@bonjourvallarta.com
+        </p>
+      </div>
+      <div class="col-md-4 text-h6 text-center">
+        <div class="">
+          <q-btn round fab-mini>
+            <q-avatar size="50px">
+              <img src="../assets/iconos/fbicon.png">
+            </q-avatar>
+          </q-btn>
+          <q-btn round fab-mini>
+            <q-avatar size="50px">
+              <img src="../assets/iconos/twicon.png">
+            </q-avatar>
+          </q-btn>
+          <q-btn round fab-mini>
+            <q-avatar size="50px">
+              <img src="../assets/iconos/yticon.png">
+            </q-avatar>
+          </q-btn>
+          <q-btn round fab-mini type="a" href="" target="__blank">
+            <q-avatar size="50px">
+              <img src="../assets/iconos/instaicon.png">
+            </q-avatar>
+          </q-btn>
+        </div>
+        <div style="font-size: 14px" class="q-pt-xl">
+          <label for=""> &copy; Copyright 2021 - Academia de lenguaje BONJOUR VALLARTA. </label><br>
+          <label for=""> Powered by: QUETZALCORE</label>
+        </div>
+      </div>
+      <div class="col-md-4 text-h6" style="padding-left: 180px">
+        <div style="font-size: 15px">
+          <q-btn style="font-size: 15px" label="Términos" flat no-caps></q-btn><br>
+          <q-btn  style="font-size: 15px" label="Privacidad" flat no-caps></q-btn><br>
+          <q-btn style="font-size: 15px" label="Ayuda" flat no-caps></q-btn><br>
+          <q-btn style="font-size: 15px" label="Desarrollador" flat no-caps></q-btn><br>
+        </div>
+      </div>
+    </div>
   </q-page>
 </template>
 <script>
