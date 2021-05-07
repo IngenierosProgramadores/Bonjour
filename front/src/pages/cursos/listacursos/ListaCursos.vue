ea63c5aa5aad49885c879e85e30fd606152bec61
Isaac <isaac.996@hotmail.com>
2021-05-06 22:51:38 -0500

    Se agrega apartado de comentarios en lista de cursos

--- front/src/pages/cursos/listacursos/ListaCursos.vue
+++ front/src/pages/cursos/listacursos/ListaCursos.vue
@@ -9,6 +9,45 @@
               :ratio="16 / 9"
               src="https://www.youtube.com/embed/k3_tw44QsZQ?rel=0"
             />
+            <!-- Se inicia el apartado de agregar comentarios -->
+            <q-card>
+              <q-card-section>
+                <div class="text-h6">Haz un comentario</div>
+              </q-card-section>
+
+              <q-separator />
+
+              <q-card-section>
+                 <q-input outlined type="textarea" label="Comentario" />
+                 <q-card-actions align="right">
+                        <q-btn flat color="deep-orange">Cancelar</q-btn>
+                        <q-btn flat color="primary">Comentar</q-btn>
+                 </q-card-actions>
+              </q-card-section>
+            </q-card>
+            <!-- Se termina el apartado de agregar comentarios -->
+            <!-- Se inicia el apartado de comentarios -->
+            <q-card>
+              <q-card-section>
+                <div class="text-h6">Comentarios</div>
+              </q-card-section>
+              <q-separator />
+
+              <q-card-section>
+                <div class="row">
+                  <div class="col-10">
+                    <div class="text-h6">Isaac Barragan</div>
+                  </div>
+                  <div class="col">
+                    <div class="text-6">Hace un dia</div>
+                  </div>
+                </div>
+                </q-card-section>
+                <q-card-section class="q-pt-none">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur in pariatur, maxime eveniet omnis sunt. Sed deleniti vel recusandae accusantium modi vitae quos ab eum exercitationem? Odit harum inventore obcaecati?
+                </q-card-section>
+
+            </q-card>
           </div>
           <!-- Card de videos del curso -->
           <div class="col q-pa-md">
@@ -16,6 +55,7 @@
               <q-card-section>
                 <div class="text-h5">Lista de videos</div>
               </q-card-section>
+              <q-separator />
               <q-list v-for="listavideos in getListaCursos" :key="listavideos.id">
                 <q-item clickable>
                   <!-- <q-item-section avatar>
